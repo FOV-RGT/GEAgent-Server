69696116f4b0b34982525fe72195dd8520eea9e9
自宅警备员 <Q1950088436@outlook.com>
2025-04-11 22:04:27 +0800

    查询所有设置 (#11)
    * 小更新 可获取所有设置

--- routes/config.js
+++ routes/config.js
@@ -5,8 +5,13 @@ const { findConfig } = require('../middleware/config');
 const configController = require('../controllers/configController');
 const { validateConfig } = require('../middleware/validators');
 
+// 获取指定LLM设置
 router.get('/:LLMID', authenticateJWT, findConfig, configController.getConfig);
 
+// 更新指定LLM设置
 router.put('/:LLMID', authenticateJWT, validateConfig, findConfig, configController.updateConfig);
+
+// 获取所有LLM设置
+router.get('/', authenticateJWT, configController.getAllConfigs);
 
 module.exports = router;
